ec9c60ed9278cc4a77d0b172f240d479b1fe044c
tjmtmmnk <tsujimoto027@gmail.com>
2019-11-03 16:41:21 +0900

    フォルダの中にブックマークを追加する

--- js/background.js
+++ js/background.js
@@ -1,3 +1,5 @@
+const BOOKMARK_FOLDER = 'Import from hatena bookmark';
+
 chrome.browserAction.onClicked.addListener((tab) => {
     chrome.bookmarks.getTree((trees) => {
         const bookmark_bars = trees[0].children;
@@ -5,24 +7,6 @@ chrome.browserAction.onClicked.addListener((tab) => {
         create_bookmark_folder(mainly_bookmark_bar_id);
     });
 });
-
-function create_bookmark_folder(bookmark_bar_id) {
-    chrome.bookmarks.search({ 'title': 'Extension bookmarks2' }, (folders) => {
-        const folders_exclude_trash = folders.filter((folder) => !folder.trash && folder.parentId == bookmark_bar_id);
-        const not_create_folder = folders_exclude_trash.length == 1;
-        if (not_create_folder) {
-            console.log("already exist");
-        } else {
-            chrome.bookmarks.create({
-                'parentId': bookmark_bar_id,
-                'title': 'Extension bookmarks2'
-            },
-                function (newFolder) {
-                    console.log("added folder: " + newFolder.title);
-                });
-        }
-    });
-}
 
 function get_mainly_bookmark_bar_id(bookmark_bars) {
     var prev = -1;
@@ -36,4 +20,35 @@ function get_mainly_bookmark_bar_id(bookmark_bars) {
         }
     });
     return mainly_bookmark_bar_id;
+}
+
+// TODO: Promise実装
+function create_bookmark_folder(bookmark_bar_id) {
+    const query_for_bookmark_folder = {
+        'title': BOOKMARK_FOLDER,
+        'url': null
+    };
+    chrome.bookmarks.search(query_for_bookmark_folder, (folders) => {
+        const folders_exclude_trash = folders.filter((folder) => !folder.trash && folder.parentId == bookmark_bar_id);
+        const create_folder = folders_exclude_trash.length == 0;
+        if (create_folder) {
+            chrome.bookmarks.create({
+                'parentId': bookmark_bar_id,
+                'title': BOOKMARK_FOLDER
+            },
+                (new_folder) => create_bookmarks(new_folder.id)
+            );
+        } else {
+            console.log("already exist");
+        }
+    });
+}
+
+function create_bookmarks(bookmark_folder_id) {
+    chrome.bookmarks.create({
+        'parentId': bookmark_folder_id,
+        'title': 'dev',
+        'url': 'https://developer.chrome.com/extensions/bookmarks#method-search'
+    }, () => console.log("create with in folder")
+    );
 }
