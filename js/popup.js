4b0c5ad405f3751c4b2acd88c86ba65d829f774b
tjmtmmnk <tsujimoto027@gmail.com>
2019-11-08 01:22:46 +0900

    Buttonは不要だった

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const select_box = document.getElementById('select-box-bookmark-bar');
-    const select_button = document.getElementById('select-button');
     const import_button = document.getElementById('import');
 
     const cached_bookmark_bar_id = await browser.storage.local.get('bookmark_bar_id');
-    if (cached_bookmark_bar_id === undefined) {
-        await chrome.runtime.sendMessage({ action: 'getBookmarkBars' });
-    } else {
-        select_box.hidden = true;
-        select_button.hidden = true;
-    }
+    await chrome.runtime.sendMessage({ action: 'getBookmarkBars' });
+    // if (cached_bookmark_bar_id.bookmark_bar_id === undefined) {
+    //     await chrome.runtime.sendMessage({ action: 'getBookmarkBars' });
+    // } else {
+    //     select_box.hidden = true;
+    // }
 
     chrome.runtime.onMessage.addListener(msg => {
         if (msg.action == "selectedBookmarkBars") {
@@ -21,17 +20,11 @@ document.addEventListener('DOMContentLoaded', async () => {
                 select_option.text = bookmark_bar.title;
                 select_box.appendChild(select_option);
             });
-
-            select_button.innerText = select_box.options[select_box.selectedIndex].text + 'を選択';
         }
     });
 
     select_box.addEventListener('change', () => {
-        select_button.innerText = select_box.options[select_box.selectedIndex].text + 'を選択';
-    });
-
-    select_button.addEventListener('click', () => {
-        const bookmark_bar_id = select_box.value;
+        const bookmark_bar_id = cached_bookmark_bar_id ? cached_bookmark_bar_id.bookmark_bar_id : select_box.value;
         chrome.runtime.sendMessage({
             action: 'setBookmarkBarID',
             id: bookmark_bar_id
